e005c7e448ef948b34e4a2e4816b7dca0b8c389c
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-03-24 14:31:00 -0400

    Update Main.hx

--- default/source/Main.hx
+++ default/source/Main.hx
@@ -5,9 +5,57 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	var game = {
+		width: 0,
+		height: 0,
+		initialState: PlayState,
+		zoom: -1.0,
+		fps: 60,
+		skipSplash: true,
+		startFullscreen: false,
+	};
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+	}
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
 	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (game.zoom == -1)
+		{
+			var ratioX:Float = stageWidth / game.width;
+			var ratioY:Float = stageHeight / game.height;
+			game.zoom = Math.min(ratioX, ratioY);
+			game.width = Math.ceil(stageWidth / game.zoom);
+			game.height = Math.ceil(stageHeight / game.zoom);
+		}
+
+		addChild(new FlxGame(game.width, game.height, game.initialState, game.fps, game.fps, game.skipSplash, game.startFullscreen));
 }
